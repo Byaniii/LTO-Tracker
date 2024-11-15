01c4de92524eb8977ae339268547caf7c0fde8f7
GonSci <igonneski@gmail.com>
2024-11-15 21:41:51 +0800

    Finished Admin Login Frame

--- main.java
+++ main.java
@@ -13,7 +13,7 @@ public class main {
 
         // Resize LOGO
         ImageIcon Icon = new ImageIcon("eto.png");
-        Image resizedImage = Icon.getImage().getScaledInstance(250,250, Image.SCALE_SMOOTH);
+        Image resizedImage = Icon.getImage().getScaledInstance(250, 250, Image.SCALE_SMOOTH);
         ImageIcon resizedIcon = new ImageIcon(resizedImage);
 
         // Center LOGO
@@ -25,27 +25,62 @@ public class main {
         JLabel centerText = new JLabel("LTO TRACKER");
         centerText.setForeground(Color.WHITE);
         centerText.setFont(new Font("Serif", Font.BOLD, 35));
-        centerText.setBounds(630,289,700,48); 
+        centerText.setBounds(630, 289, 700, 48);
         newFrame.bodyPanel.add(centerText);
 
         // Subtitle text: "KEEPING OUR ROADS SAFE"
         JLabel subtitleLabel = new JLabel("\"KEEPING OUR ROADS SAFE\"", SwingConstants.CENTER);
         subtitleLabel.setForeground(Color.WHITE);
         subtitleLabel.setFont(new Font("Serif", Font.BOLD, 40));
-        subtitleLabel.setBounds(455,364,590,53);
+        subtitleLabel.setBounds(455, 364, 590, 53);
         newFrame.bodyPanel.add(subtitleLabel);
 
-
-        
         CustomButton adminButton = newFrame.addButtonToBodyPanel("ADMIN", 400, 450, 300, 80);
         adminButton.addActionListener(e -> {
-            AdminFrame useFrame = new AdminFrame();  
+            AdminLogin();
         });
 
         CustomButton userButton = newFrame.addButtonToBodyPanel("USER", 800, 450, 300, 80);
-        userButton.addActionListener(e -> {  
+        userButton.addActionListener(e -> {
         });
+    }
+    
+    private static void AdminLogin() {
+        frame newFrame = new frame("Login");
+        newFrame.bodyPanel.setBackground(Color.white);
 
+        //Centered Admin text
+        JLabel Admin_Label = new JLabel("ADMIN");
+        Admin_Label.setBounds(670, 80, 200, 70);
+        Admin_Label.setFont(new Font("Serif", Font.BOLD, 40));
+        Admin_Label.setForeground(Color.red);
+        newFrame.bodyPanel.add(Admin_Label);
 
+        //Username text
+        JLabel Username_Label = new JLabel("USERNAME");
+        Username_Label.setFont(new Font("Serif", Font.BOLD, 24));
+        Username_Label.setBounds(300, 220, 300, 30);
+        newFrame.bodyPanel.add(Username_Label);
+
+        //Text field for Username
+        CustomTextField Username_Field = CustomTextField.createTextField(470, 213, 550, 50);
+        newFrame.bodyPanel.add(Username_Field);
+
+        //Password Text
+        JLabel Password_Label = new JLabel("PASSWORD");
+        Password_Label.setFont(new Font("Serif", Font.BOLD, 24));
+        Password_Label.setBounds(297, 313, 300, 30);
+        newFrame.bodyPanel.add(Password_Label);
+
+        //Text field for Password
+        CustomTextField Password_Field = CustomTextField.createTextField(470, 306, 550, 50);
+        newFrame.bodyPanel.add(Password_Field);
+
+        //Sample Login button for testing lang
+        CustomButton Login_Button = newFrame.addButtonToBodyPanel("LOGIN", 878, 385, 143, 37);
+        Login_Button.setForeground(Color.white);
+        Login_Button.setFont(new Font("Arial", Font.BOLD,20));
+        Login_Button.setBackground(Color.red);
+        Login_Button.setBorder(null);
     }
 }
