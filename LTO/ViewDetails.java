c698c39fa61de3c0e8ad40c15663a4ca639f068f
GonSci <igonneski@gmail.com>
2024-11-24 02:09:02 +0800

    Added main menu button

--- LTO/ViewDetails.java
+++ LTO/ViewDetails.java
@@ -55,7 +55,6 @@ public class ViewDetails extends JFrame {
 
         add(footerPanel, BorderLayout.SOUTH);
 
-        // Load the Profile Details
         loadProfileView();
         setVisible(true);
     }
